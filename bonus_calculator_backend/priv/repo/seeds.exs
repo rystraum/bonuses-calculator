8453b3991b457d0249b7cd1d5620e123bf46cf65
Rystraum Gamonez <rystraum@gmail.com>
2026-09-20 02:13:08 +0800

    style(backend): format seeds.exs

--- bonus_calculator_backend/priv/repo/seeds.exs
+++ bonus_calculator_backend/priv/repo/seeds.exs
@@ -3,7 +3,10 @@ alias BonusCalculatorBackend.Accounts
 case Accounts.get_user_by_username("rystraum@personal-it-y.com") do
   nil ->
     {:ok, _user} =
-      Accounts.create_user(%{"username" => "rystraum@personal-it-y.com", "password" => "pass.123"})
+      Accounts.create_user(%{
+        "username" => "rystraum@personal-it-y.com",
+        "password" => "pass.123"
+      })
 
   _user ->
     :ok
