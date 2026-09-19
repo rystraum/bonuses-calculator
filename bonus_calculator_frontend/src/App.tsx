0d9f147bdc6a20e072682c103ff90f65c0ad177e
Rystraum Gamonez <rystraum@gmail.com>
2026-09-19 20:17:16 +0800

    feat(frontend): routes for /compare and /import

--- bonus_calculator_frontend/src/App.tsx
+++ bonus_calculator_frontend/src/App.tsx
@@ -6,6 +6,8 @@ import Distributions from '@/pages/Distributions'
 import DistributionDetail from '@/pages/DistributionDetail'
 import Shareholders from '@/pages/Shareholders'
 import Employees from '@/pages/Employees'
+import Compare from '@/pages/Compare'
+import Import from '@/pages/Import'
 
 export default function App() {
   return (
@@ -14,8 +16,10 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<RequireAuth><Distributions /></RequireAuth>} />
         <Route path="/distributions/:id" element={<RequireAuth><DistributionDetail /></RequireAuth>} />
+        <Route path="/compare" element={<RequireAuth><Compare /></RequireAuth>} />
         <Route path="/shareholders" element={<RequireAuth><Shareholders /></RequireAuth>} />
         <Route path="/employees" element={<RequireAuth><Employees /></RequireAuth>} />
+        <Route path="/import" element={<RequireAuth><Import /></RequireAuth>} />
         <Route path="*" element={<RequireAuth><Distributions /></RequireAuth>} />
       </Routes>
     </StoreProvider>
