a5cff9c50633a98525e8dd935e87357dfd461f86
Rystraum Gamonez <rystraum@gmail.com>
2026-09-19 19:48:30 +0800

    feat(backend): add DistributionGroupMemberController (hours/multiplier update)

---
+++ bonus_calculator_backend/lib/bonus_calculator_backend_web/controllers/distribution_group_member_controller.ex
@@ -0,0 +1,25 @@
+defmodule BonusCalculatorBackendWeb.DistributionGroupMemberController do
+  use BonusCalculatorBackendWeb, :controller
+
+  alias BonusCalculatorBackend.Distributions
+  alias BonusCalculatorBackend.Distributions.DistributionGroupMember
+
+  action_fallback BonusCalculatorBackendWeb.FallbackController
+
+  def update(conn, %{"id" => id} = params) do
+    member = Distributions.get_member!(id)
+
+    with {:ok, %DistributionGroupMember{} = member} <- Distributions.update_member(member, params) do
+      json(conn, %{
+        data: %{
+          id: member.id,
+          distribution_group_id: member.distribution_group_id,
+          employee_id: member.employee_id,
+          employee_name: member.employee_name,
+          hours: Decimal.to_string(member.hours, :normal),
+          performance_multiplier: Decimal.to_string(member.performance_multiplier, :normal)
+        }
+      })
+    end
+  end
+end
